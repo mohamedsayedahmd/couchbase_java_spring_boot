e9554ef2764990a98c70b3db43c54e982585d291
mohamedsayedahmd <83403632+mohamedssed@users.noreply.github.com>
2023-08-18 14:07:41 +0300

    add findByEmail

--- src/main/java/com/customer/manage/repo/UserRepo.java
+++ src/main/java/com/customer/manage/repo/UserRepo.java
@@ -2,8 +2,12 @@ package com.customer.manage.repo;
 
 import com.customer.manage.model.User;
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
+import org.springframework.data.couchbase.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends CouchbaseRepository<User,Integer> {
+    @Query("#{#n1ql.selectEntity} WHERE email = $1")
+    User findByEmail(String email);
+
 }
