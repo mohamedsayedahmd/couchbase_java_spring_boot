9d5be7ab816c88d620c7a05da8b5cdb391faf05b
mohamedsayedahmd <83403632+mohamedssed@users.noreply.github.com>
2023-08-18 14:10:58 +0300

    create new package with the name exceptions and add UserNotFoundExceptions.java

---
+++ src/main/java/com/customer/manage/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.customer.manage.exceptions;
+
+public class UserNotFoundException extends Exception{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
